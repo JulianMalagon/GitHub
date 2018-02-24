90f4db44466e79d9279cd39b3d06b63ecbc303f1
Julian Malagon <jmalagon100@gmail.com>
2018-02-23 19:08:43 -0500

    CSharp

---
+++ CSharp/HolaMundo.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+namespace HolaMundo
+  {
+    class Program
+      {
+        static void Main (String[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            int Edad = 25;
+            Console.WriteLine(Edad);
+
+            Console.Read();
+
+        }
+    }
+}
